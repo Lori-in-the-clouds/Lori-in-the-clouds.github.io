914fcad9ded6a54c539d35836da9b9058aeaec21
Lorenzo Di Maio <lorenzodimaio02@gmail.com>
2026-03-08 23:52:04 +0100

    site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  // AGGIUNGI QUESTE DUE RIGHE:
+  site: 'https://lori-in-the-clouds.github.io',
+  base: '/', 
+  
   vite: {
     plugins: [tailwindcss()]
   },
